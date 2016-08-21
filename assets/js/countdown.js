fa221931bfe98c5b86991f2605e9dce1af0d06dd
ryanc23 <ryan@myvidster.com>
2016-08-21 17:52:17 -0500

    revert

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,14 +1,74 @@
-setInterval(function () {
 
-var enddate = new Date("Sep 02 2016 00:00:00 GMT+05:30"); // change your date here like Jan 10 2015
+/**
+* Metis - Bootstrap-Admin-Template v2.2.2
+* Author : Osman Nuri Okumus 
+* Copyright 2014
+* Licensed under MIT
+*/
+/* Start Countdown Settings */
 
-var today = new Date();
+var startDate = new Date("16/03/2014"); //THIS IS JUST FOR REFERENCE- DO NOT CHANGE THIS.
 
-var difference = Math.floor((enddate.getTime() - today.getTime()) / 1000);
+var endDate = new Date("02/09/2016"); //CHANGE THIS TO YOUR LAUNCHING DATE
 
-var seconds = GlobalFunctn(difference % 60);
+var dif = endDate.getTime() - startDate.getTime();
+var difToSecond = dif / 1000;
+var defaultPercent = 0;
 
 
-difference = Math.floor(difference / 60);
+$(function() {
+    $('#counter').countdown({
+        until: endDate,
+        layout: '<div></div>',
+        onTick: updateBar
+    });
 
+    $('a[rel=tooltip]').tooltip();
+    $('div[rel=tooltip]').tooltip();
 });
+
+function updateBar(periods) {
+
+    fillSecondBar(periods[6]);
+    fillMinuteBar(periods[5]);
+    fillHourBar(periods[4]);
+    fillDayBar(periods[3]);
+
+    fillTotalbar(periods[6] + periods[5] * 60 + periods[4] * 60 * 60 + periods[3] * 60 * 60 * 24);
+}
+
+function fillSecondBar(percent) {
+    $('#second-number').html(percent);
+    $('#second-bar').css('width', percent * 100 / 60 + '%');
+}
+
+function fillMinuteBar(percent) {
+    $('#minute-number').html(percent);
+    $('#minute-bar').css('width', percent * 100 / 60 + '%');
+}
+
+function fillHourBar(percent) {
+    $('#hour-number').html(percent);
+    $('#hour-bar').css('width', percent * 100 / 24 + '%');
+}
+
+function fillDayBar(percent) {
+    $('#day-number').html(percent);
+    $('#day-bar').css('width', percent * 100 / 365 + '%');
+}
+
+function fillTotalbar(percent) {
+    defaultPercent = 100 - 100 * percent / difToSecond;
+
+    if (defaultPercent >= 10) {
+        currentPercent = defaultPercent.toString().substr(0, 5);
+    } else {
+        currentPercent = defaultPercent.toString().substr(0, 4);
+    }
+
+    $('#total-bar').css('width', defaultPercent + '%').html(currentPercent + '%');
+}
+
+
+
+
